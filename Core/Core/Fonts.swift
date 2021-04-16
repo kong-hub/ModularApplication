df3f4d4518c788a80d1c6e34e42beb37e6a9091a
Sorawit Trutsat <sorawit.trutsat@gmail.com>
2021-04-16 16:25:13 +0700

    Added Fonts into Core Module

--- Core/Core/Fonts.swift
+++ Core/Core/Fonts.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import CoreText
+import UIKit
 
 public enum Fonts: String, CaseIterable {
     case robotoBlackItalic = "Roboto-BlackItalic"
@@ -14,4 +16,28 @@ public enum Fonts: String, CaseIterable {
     case robotoBold = "Roboto-Bold"
     
     static var installed = false
+}
+
+public extension Fonts {
+    static func install(from bundles: [Bundle] = []) {
+        Fonts.installed = true
+        for each in Fonts.allCases {
+            for bundle in bundles {
+                if let cfURL = bundle.url(forResource: each.rawValue, withExtension: "ttf") {
+                    CTFontManagerRegisterFontsForURL(cfURL as CFURL, .process, nil)
+                } else {
+                    assertionFailure("Could not find font:\(each.rawValue) in bundle:\(bundle)")
+                }
+            }
+        }
+    }
+}
+
+public extension Fonts {
+    func size(_ size : CGFloat) -> UIFont {
+        if Fonts.installed == false {
+            Fonts.install()
+        }
+        return UIFont(name: self.rawValue, size:  size)!
+    }
 }
