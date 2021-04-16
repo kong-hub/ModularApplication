3ec7dac31162920de9aafbb9b61ce8f27f2bc854
Sorawit Trutsat <sorawit.trutsat@gmail.com>
2021-04-16 16:14:35 +0700

    Added Fonts to Core Module

---
+++ Core/Core/Fonts.swift
@@ -0,0 +1,17 @@
+//
+//  Fonts.swift
+//  Core
+//
+//  Created by Sorawit Trutsat on 16/4/2564 BE.
+//
+
+import Foundation
+
+public enum Fonts: String, CaseIterable {
+    case robotoBlackItalic = "Roboto-BlackItalic"
+    case robotoBlack = "Roboto-Black"
+    case robotoBoldItalic = "Roboto-BoldItalic"
+    case robotoBold = "Roboto-Bold"
+    
+    static var installed = false
+}
