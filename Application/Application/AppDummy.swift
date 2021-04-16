f8f9e117416f2cb23a44e66a8411a9b5417d16e0
Sorawit Trutsat <sorawit.trutsat@gmail.com>
2021-04-16 15:34:02 +0700

    Added AppDummy class

---
+++ Application/Application/AppDummy.swift
@@ -0,0 +1,14 @@
+//
+//  AppDummy.swift
+//  Application
+//
+//  Created by Sorawit Trutsat on 16/4/2564 BE.
+//
+
+import Foundation
+
+public class AppDummy {
+    public init() {
+        
+    }
+}
--- Core/CorePlayground.playground/Contents.swift
+++ Core/CorePlayground.playground/Contents.swift
@@ -1,4 +1,6 @@
 import UIKit
 import Core
+import RxSwift
 
+Observable<Int>.just(2)
 let dummy = CoreDummy()
