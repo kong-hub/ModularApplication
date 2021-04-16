d406a7dd41ec7687f622d65407cd38673fa22444
Sorawit Trutsat <sorawit.trutsat@gmail.com>
2021-04-16 15:47:58 +0700

    Added ProductListDummy class amd Playground

---
+++ ProductList/ProductList/ProductListDummy.swift
@@ -0,0 +1,14 @@
+//
+//  ProductListDummy.swift
+//  ProductList
+//
+//  Created by Sorawit Trutsat on 16/4/2564 BE.
+//
+
+import Foundation
+
+public class ProductListDummy {
+    public init() {
+        print("Initialize ProductListDummy")
+    }
+}
---
+++ ProductList/ProductListPlayground.playground/Contents.swift
@@ -0,0 +1,4 @@
+import UIKit
+import ProductList
+
+let productListDummy = ProductListDummy()
